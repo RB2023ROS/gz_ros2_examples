f8c18c318b688d8fc415a3ff139e1096e8ce1bcf
kimsooyoung <tge1375@naver.com>
2023-04-14 19:48:02 +0900

    left works

--- chess_world/launch/chess_world.launch.py
+++ chess_world/launch/chess_world.launch.py
@@ -58,26 +58,33 @@ def generate_launch_description():
     start_gazebo_client_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py'))
     )
-    
-    # # Get URDF via xacro
-    # robot_description_content = Command(
-    #     [
-    #         PathJoinSubstitution([FindExecutable(name="xacro")]),
-    #         " ",
-    #         PathJoinSubstitution(
-    #             [FindPackageShare("chess_world"), "urdf", "sensor_stick.urdf.xacro"]
-    #         ),
-    #     ]
-    # )
 
-    # robot_description = {"robot_description": robot_description_content}
+    static_transform_publisher = Node(
+        package = "tf2_ros",
+        executable = "static_transform_publisher",
+        arguments = ["0", "0", "0", "0", "0", "0", "world", "chess_frame"],
+        condition=IfCondition(LaunchConfiguration("start_rviz"))
+    )
 
-    # robot_state_publisher = Node(
-    #     package='robot_state_publisher',
-    #     executable='robot_state_publisher',
-    #     output='screen',
-    #     parameters=[robot_description]
-    # )
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("chess_world"), "urdf", "camera_left.urdf.xacro"]
+            ),
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
 
     # Spawn Robot
     spawn_entity = Node(
@@ -85,7 +92,7 @@ def generate_launch_description():
         executable='spawn_entity.py',
         arguments=[
             '-topic', 'robot_description',
-            '-entity', 'sensor_stick',
+            '-entity', 'd435_camera',
             '-x', str(0),
             '-y', str(0.0),
             '-Y', str(0.0),
@@ -106,8 +113,10 @@ def generate_launch_description():
     return LaunchDescription(
         [
             arg_show_rviz,
-            start_gazebo_server_cmd,
-            start_gazebo_client_cmd,
+            # start_gazebo_server_cmd,
+            # start_gazebo_client_cmd,
+            # static_transform_publisher,
+            robot_state_publisher,
             # spawn_entity,
             # RegisterEventHandler(
             #     event_handler=OnProcessExit(
