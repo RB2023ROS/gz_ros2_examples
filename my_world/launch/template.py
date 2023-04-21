dc5e9310c8dfe3dba91ec0bc56d58c23d4f07735
kimsooyoung <tge1375@naver.com>
2023-04-21 21:12:17 +0900

    my world

--- my_world/launch/template.py
+++ my_world/launch/template.py
@@ -13,19 +13,34 @@
 # limitations under the License.
 
 import os
+from osrf_pycommon.terminal_color import ansi
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler
 
+from launch.conditions import IfCondition
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch_ros.actions import Node
+
+from launch.event_handlers import OnProcessExit
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
 
-    pkg_path = os.path.join(get_package_share_directory('tf2_world'))
-    world_path = os.path.join(pkg_path, 'worlds', 'orbit.world')
+    # gz model path edit
+    gazebo_model_path = os.path.join(get_package_share_directory('my_world'), 'models')
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] += ":" + gazebo_model_path
+    else :
+        os.environ['GAZEBO_MODEL_PATH'] = gazebo_model_path
+    print(ansi("yellow"), "If it's your 1st time to download Gazebo model on your computer, it may take few minutes to finish.", ansi("reset"))
+
+    pkg_path = os.path.join(get_package_share_directory('my_world'))
+    # world_path = os.path.join(pkg_path, 'worlds', 'orbit.world')
+    
     pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')   
     
     # Start Gazebo server
@@ -39,16 +54,10 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py'))
     )
 
-    static_transform_publisher = Node(
-        package = "tf2_ros",
-        executable = "static_transform_publisher",
-        arguments = ["0", "0", "0.5", "0", "0", "0", "world", "unit_sphere"],
-    )
 
     return LaunchDescription(
         [
             start_gazebo_server_cmd,
             start_gazebo_client_cmd,
-            # static_transform_publisher,
         ]
     )
